ef2f1a7b25d30fe4d37ff1af83f684b5bec0257e
Scott <scottantwi930@gmail.com>
2026-02-08 04:21:18 +0000

    Refactor pathfinding algorithms and improve readability

--- pathfinder.py
+++ pathfinder.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import numpy as np
-
-# 0 = free space, 1 = wall
+# 0 = free, 1 = wall
 grid = [
     [0,0,0,0,1,0,0],
     [1,1,0,0,1,0,1],
@@ -19,26 +15,26 @@ cols = len(grid[0])
 
 # BFS
 def bfs_search():
-    q = [start]  # used list instead of deque, humans sometimes forget deque
+    queue = [start]
     parents = {start: None}
     visited_nodes = []
 
-    while q:
-        current = q.pop(0)  # humans pop 0 from list
+    while queue:
+        current = queue.pop(0)
         visited_nodes.append(current)
 
         if current == goal:
             break
 
-        for dr,dc in [(1,0), (-1,0), (0,1), (0,-1)]:
-            new_r = current[0]+dr
-            new_c = current[1]+dc
-            neighbor = (new_r,new_c)
+        for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+            r_new = current[0] + dr
+            c_new = current[1] + dc
+            neighbor = (r_new, c_new)
 
-            if 0 <= new_r < rows and 0 <= new_c < cols:
-                if grid[new_r][new_c]==0 and neighbor not in parents:
-                    parents[neighbor] = current
-                    q.append(neighbor)
+            if 0 <= r_new < rows and 0 <= c_new < cols and grid[r_new][c_new]==0 and neighbor not in parents:
+                parents[neighbor] = current
+                queue.append(neighbor)
+
     path = []
     cur = goal
     while cur is not None:
@@ -60,39 +56,37 @@ def dfs_search():
         if n == goal:
             break
 
-        for dr,dc in [(0,1),(1,0),(-1,0),(0,-1)]:  # different order, humans do this
-            r_new = n[0]+dr
-            c_new = n[1]+dc
+        for dr, dc in [(0,1),(1,0),(-1,0),(0,-1)]:
+            r_new = n[0] + dr
+            c_new = n[1] + dc
             neigh = (r_new,c_new)
 
-            if r_new>=0 and r_new<rows and c_new>=0 and c_new<cols:
-                if grid[r_new][c_new]==0 and neigh not in parent_map:
-                    parent_map[neigh]=n
-                    stack.append(neigh)
-    # reconstruct path slightly different
+            if 0 <= r_new < rows and 0 <= c_new < cols and grid[r_new][c_new]==0 and neigh not in parent_map:
+                parent_map[neigh] = n
+                stack.append(neigh)
+
     path = [goal]
     while path[-1] in parent_map and parent_map[path[-1]] is not None:
         path.append(parent_map[path[-1]])
     path.reverse()
     return visited, path
 
-# A*
+# A* search
 def astar_search():
     open_list = [(0,start)]
     costs = {start:0}
-    parents = {start:None}
+    parents = {start: None}
     visited_nodes = []
 
     def manhattan(a,b):
-        return abs(a[0]-b[0])+abs(a[1]-b[1])
+        return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
     while open_list:
-        # find min manually instead of heapq, humans sometimes do this
         open_list.sort()
         current = open_list.pop(0)[1]
         visited_nodes.append(current)
 
-        if current==goal:
+        if current == goal:
             break
 
         for move in [(1,0),(0,1),(-1,0),(0,-1)]:
@@ -100,48 +94,54 @@ def astar_search():
             c = current[1]+move[1]
             neighbor = (r,c)
 
-            if 0<=r<rows and 0<=c<cols and grid[r][c]==0:
-                new_cost = costs[current]+1
-                if neighbor not in costs or new_cost<costs[neighbor]:
-                    costs[neighbor]=new_cost
-                    f = new_cost+manhattan(neighbor,goal)
-                    open_list.append((f,neighbor))
-                    parents[neighbor]=current
+            if 0 <= r < rows and 0 <= c < cols and grid[r][c]==0:
+                new_cost = costs[current] + 1
+                if neighbor not in costs or new_cost < costs[neighbor]:
+                    costs[neighbor] = new_cost
+                    priority = new_cost + manhattan(neighbor,goal)
+                    open_list.append((priority, neighbor))
+                    parents[neighbor] = current
 
-    # path reconstruction, a bit different
-    p = []
+    path = []
     cur = goal
     while cur:
-        p.append(cur)
+        path.append(cur)
         cur = parents.get(cur)
-    p.reverse()
-    return visited_nodes, p
+    path.reverse()
+    return visited_nodes, path
+def print_grid(path):
+    for r in range(rows):
+        row_str = ""
+        for c in range(cols):
+            if (r,c) == start:
+                row_str += "S "
+            elif (r,c) == goal:
+                row_str += "G "
+            elif (r,c) in path:
+                row_str += "* "
+            elif grid[r][c] == 1:
+                row_str += "# "
+            else:
+                row_str += ". "
+        print(row_str)
+    print("\n")
+# BFS search
+b_vis, b_path = bfs_search()
+print("\nBFS Pathfinding:")
+print("Visited nodes count:", len(b_vis))
+print("Path length:", len(b_path))
+print_grid(b_path)
 
-# Visualization
-def animate_search(vis_order,path,title):
-    mat = np.array(grid,dtype=float)
-    fig, ax = plt.subplots()
-    img = ax.imshow(mat,cmap='gray_r')
+# DFS search
+d_vis, d_path = dfs_search()
+print("\nDFS Pathfinding:")
+print("Visited nodes count:", len(d_vis))
+print("Path length:", len(d_path))
+print_grid(d_path)
 
-    def update(frame):
-        r,c = vis_order[frame]
-        mat[r][c]=0.5
-        img.set_data(mat)
-        return img,
-
-    ani = animation.FuncAnimation(fig, update, frames=len(vis_order),
-                                  interval=150, repeat=False)
-
-    for r,c in path:
-        mat[r][c]=0.2
-
-    ax.set_title(title)
-    plt.show()
-
-b_vis,b_path = bfs_search()
-d_vis,d_path = dfs_search()
-a_vis,a_path = astar_search()
-
-animate_search(b_vis,b_path,"BFS Pathfinding")
-animate_search(d_vis,d_path,"DFS Pathfinding")
-animate_search(a_vis,a_path,"A* Pathfinding")
+# A*
+a_vis, a_path = astar_search()
+print("\nA* Pathfinding:")
+print("Visited nodes count:", len(a_vis))
+print("Path length:", len(a_path))
+print_grid(a_path)
